6fb402ca903c698adc55163edc69438200fdb6f9
DariusRain <dariusrain.student@careerdevs.com>
2020-06-04 18:08:50 -0400

    Though I had mergeSort.js in the last commit, fixing now.

---
+++ mergeSort.js
@@ -0,0 +1,34 @@
+// Merge Sort
+// O(n log n)
+// https://www.geeksforgeeks.org/merge-sort/
+// Like Quick sort, merge sort is a divide and conquer algorithm.
+
+// Find the middle of the array
+// sort from 
+
+// Process of merge sort 
+
+// Create a function with arguments ARRAY, LEFT, RIGHT
+// ARRAY = the array that is on the verge of being sorted
+
+// LEFT = an array, specificly the left half of the array
+// RIGHT =  vice versa to the value of LEFT
+
+// Inside the function
+
+// 1: Find the middle of the array
+// 2: Call recursively for the right half
+// 3: Call recusively for the ri
+
+const mockData = [10, 12, 8, 16, 70, 80, 15, 45, 80, 63, 1002, 8, 11, 12, 9, 5, 8, 29, 41, 52, 57, 45, 0, 6, 1003, 9, 17, 31];
+
+const mergeSort = (arr) => {
+    
+    const middle = arr.length() / 2;
+
+    if (middle > 2) return;
+
+       
+
+
+}
