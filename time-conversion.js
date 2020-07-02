e56270a9b73a73e460017f08f0106efcef307ceb
DariusRain <dariusrain.student@careerdevs.com>
2020-07-01 23:21:48 -0400

    Solved the time conversion algorithm

--- time-conversion.js
+++ time-conversion.js
@@ -2,13 +2,31 @@
 // https://www.hackerrank.com/challenges/time-conversion/problem
 
 function timeConversion(s) {
-    const isPm = s.indexOf("PM");
-    const hour = s.split(":")[0];
-    const minutes = s.split(":")[1];
-    const seconds = s.split(":")[2].substring(0,2);
-    const militaryTime = `${isPm ? parseInt(hour) + 12: hour}:${minutes}:${seconds}`;
-    return militaryTime;
+  const isPm = s.indexOf("PM") > 0 ? true : false;
+  const minutes = s.split(":")[1];
+  const seconds = s.split(":")[2].substring(0, 2);
+  let hours = s.split(":")[0];
+  if (isPm != true && hours === "12") {
+    hours = "00";
+  }
+  if (isPm > 0) {
+    hours = hours === "12" ? "12" : parseInt(hours) + 12;
+  }
+  const militaryTime = `${hours}:${minutes}:${seconds}`;
+  return militaryTime;
 }
 
 //DEBUG
-console.log(timeConversion("12:00:00AM"))
+console.log(timeConversion("12:45:54PM"));
+
+// DEBUG (Moved)
+// console.log(
+//     "Hours:",
+//     hours,
+//     "\nMinutes:",
+//     minutes,
+//     "\nSeconds:",
+//     seconds,
+//     "\nBool Test:",
+//     "None"
+//   );
