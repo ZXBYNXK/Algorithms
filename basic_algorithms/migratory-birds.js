4cfd2166ac7e36a80172963afef2c20e237b30bc
DariusRain <dariusrain.student@careerdevs.com>
2020-07-10 05:48:48 -0400

    Cleaned up something

--- basic_algorithms/migratory-birds.js
+++ basic_algorithms/migratory-birds.js
@@ -5,7 +5,7 @@ function migratoryBirds(array) {
   // birdHouse is {"BIRD-TYPE": "QTY-OF-BIRDS"} ex: {"Bird type 2": "4 of them"} really: {"2":"4", ... "6":"2"}
   // Create empty birdhouse or object
   let birdHouse = {};
-  
+
   // Set the lowest common number by default to the first value of the array.
   let lowestCommonNumber = array[0] || false;
 
@@ -22,26 +22,20 @@ function migratoryBirds(array) {
       //   console.log(i, birdHouse)
 
       // If the current bird type or number has a higher qty of birds then it is now the LCN.
-      if (birdHouse[lowestCommonNumber] < birdHouse[array[i]]) {
-
+      if (
+        birdHouse[lowestCommonNumber] < birdHouse[array[i]] ||
+        (birdHouse[lowestCommonNumber] === birdHouse[array[i]] &&
+          array[i] < lowestCommonNumber)
+      ) {
         // Assign the new LCN.
         lowestCommonNumber = array[i];
-        
-        // else if the qtys are equal and the bird type or number is lower than the current LCN then 
+
+        // else if the qtys are equal and the bird type or number is lower than the current LCN then
         // the bird type is again recognized as the lowest common number.
-      } else if (
-        birdHouse[lowestCommonNumber] === birdHouse[array[i]] &&
-        array[i] < lowestCommonNumber
-      ) {
-        // LCN has been reassinged to its predecessor.
-        lowestCommonNumber = array[i];
       }
-
     }
-
-    // Return the number left after the loop.
-    return lowestCommonNumber;
   }
+  // Return the number left after the loop.
+  return lowestCommonNumber;
 }
-
 console.log(migratoryBirds([1, 4, 4, 4, 5, 3]));
